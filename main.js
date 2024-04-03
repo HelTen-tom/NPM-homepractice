a8f5945afe7109f85be354e26ffc0e3f9f64bbf3
HelTen-tom <142582424+HelTen-tom@users.noreply.github.com>
2024-04-03 16:22:34 +0300

    add proj

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import "./style.css";
 const LOCALSTORAGE__KEY = "todos";
 const refs = {
   formToDo: document.querySelector(".form-todo"),
+  listEl: document.querySelector(".list"),
 };
 
 refs.formToDo.addEventListener("submit", onBtnClick);
@@ -15,13 +16,20 @@ function onBtnClick(event) {
   formDataValue.forEach((value, name) => {
     data[name] = value;
   });
-  console.log(data);
-  localStorage.setItem(LOCALSTORAGE__KEY, JSON.stringify(data));
+  // localStorage.setItem(LOCALSTORAGE__KEY, JSON.stringify(data));
   formEl.reset();
-};
+  handleAddCard(data);
+}
+function handleAddCard(card) {
+  refs.listEl.insertAdjacentHTML("beforeend", creatCard(card));
+  const cards = JSON.parse(localStorage.getItem(LOCALSTORAGE__KEY)) || [];
+cards.push(card);
+console.log(cards);
+localStorage.setItem(LOCALSTORAGE__KEY, JSON.stringify(cards))
+}
 function creatCard(card) {
   return `<li class="list-item">
-  <h2 class="list-item-title"></h2>
-  <p class="list-item-description"></p>
-</li>`
- }
+  <h2 class="list-item-title">${card.title}</h2>
+  <p class="list-item-description">${card.description}</p>
+</li>`;
+}
