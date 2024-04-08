ec3760d020cbb2490faa66a4c0a207df26216d8e
HelTen-tom <142582424+HelTen-tom@users.noreply.github.com>
2024-04-08 17:47:58 +0300

    add proj

--- main.js
+++ main.js
@@ -8,3 +8,9 @@ const todos = apiLocalStorage.get(LOCALSTORAGE__KEY) || [];
 
 refs.listEl.innerHTML = createCardList(todos).join("");
 refs.formToDo.addEventListener("submit", onBtnClick);
+console.log(refs.filterTitle);
+refs.filterTitle.addEventListener('input', (event)=>{
+  const{value} = event.currentTarget;
+console.log(event.currentTarget.value);
+console.log(todos.filter((item) => item.title.includes(value)));
+});
